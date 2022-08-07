1ca34015c3fc607ce4706d7dc991baaefb7f47ba
Сабака Сутулая <40202917+kapertdog@users.noreply.github.com>
2022-08-07 21:51:39 +0500

    я собака...

--- updater/install_requirements_to_new.cmd
+++ updater/install_requirements_to_new.cmd
@@ -1,6 +1,7 @@
 echo Off
 cls
-echo Make sure that you have Python3.10!!!
-pause
+echo ## Make sure that you have Python3.10!!!
+echo ## Creating new venv...
 py -m venv ".\updater\waba_additional_files\venv"
+echo ## Installing requirements...
 ".\updater\waba_additional_files\venv\Scripts\pip.exe" install -r ".\updater\requirements.txt"
